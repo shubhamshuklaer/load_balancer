33b41d956f83682c6b1de03d8b1e7222af51d5fd
Shubham Shukla <shubhamshuklaerssss@gmail.com>
2016-03-13 13:50:00 +0530

    Added ability for specifying number of tokens to send

--- client.py
+++ client.py
@@ -5,17 +5,20 @@ import getopt
 import sys
 from twisted.internet import reactor
 
+num_tokens=10
+ip=None
+
 def usage():
     print("TODO")
 
 def get_test_token_list():
     ret_val=[]
-    for i in range(50):
+    for i in range(num_tokens):
         ret_val.append(User_token(i))
     return ret_val
 
 try:
-    opts,args=getopt.getopt(sys.argv[1:],"h",["ip="])
+    opts,args=getopt.getopt(sys.argv[1:],"hn:",["ip="])
 except getopt.GetoptError:
     usage()
     exit(2)
@@ -24,8 +27,13 @@ for opt,arg in opts:
     if opt=="-h":
         usage()
         exit(0)
+    elif opt=="-n":
+        num_tokens=int(arg)
     elif opt=="--ip":
-        run_token_client(arg,get_test_token_list())
-        if not reactor.running:
-            reactor.run()
+        ip=arg
+
+if ip != None:
+    run_token_client(ip,get_test_token_list())
+    if not reactor.running:
+        reactor.run()
 
