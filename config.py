eb621c1cfea92171a85e684e7548fe1973a6b782
Shubham Shukla <shubhamshuklaerssss@gmail.com>
2016-03-21 22:52:06 +0530

    added why we need seperate port for solution serv

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
+
+# We are using seperate port for solved_tokens so that we can run both client
+# and host on the same ip, we cannot run 2 serivces on same port.
 solved_token_serv_port=8008
 token_serv_port=8007
